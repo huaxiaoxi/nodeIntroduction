fe946acf3bcb9ea895529e097ae6056911f28b77
huaxiaoxi <644423850@qq.com>
2017-03-02 14:50:26 +0800

    Add files via upload

---
+++ requestHandlers.js
@@ -0,0 +1,77 @@
+var exec = require("child_process").exec;
+var querystring = require("querystring");
+var fs = require("fs");
+var formidable = require("formidable");
+
+function start(response){
+	console.log("Request handler 'start' was called.");
+	//var content =  "Hello Start";
+
+	exec("ls -lah",function(error,stdout,stderr){
+		//content = stdout;
+		response.writeHead(200,{"Content-Type":"text/plain"});
+		response.write(stdout);
+		response.end();
+	});
+
+	//return content;
+
+};
+
+function upload(response,request){
+	console.log("Request handler 'upload' was called");
+	//return "Hello Upload";
+
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request,function(error,fields,files){
+		console.log("parsing done");
+		fs.renameSync(files.upload.path,"/tmp/test.png");
+		response.writeHead(200,{"Content-Type":"text/html"});
+		response.write("received image<br/>");
+		response.write("<img src='/show' />");
+		response.end();
+	});
+// 	response.writeHead(200,"Content-Type");
+// 	response.write("you have sent:" + querysting.parse(postData).text);
+// 	response.end();
+};
+
+function show(response){
+	console.log("Request handler 'show' was called.");
+	fs.readFile("/tmp/test.png","binary",function(error,file){
+		if(error){
+			response.writeHead(500,{"Content-Type":"text/plain"});
+			response.write(error + "\n");
+			response.end();
+		}else{
+			response.writeHead(200, {"Content-Type": "image/png"});
+      		response.write(file, "binary");
+      		response.end();
+		}
+	});
+}
+
+function begin(response){
+	var body =  '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write(body);
+    response.end();
+};
+
+exports.start = start;
+exports.upload = upload;
+exports.begin = begin;
+exports.show = show;
